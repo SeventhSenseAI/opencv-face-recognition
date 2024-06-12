ba9970d84e33ba314def223ba5420b92c3a83de5
Fu Yi <154571636+SeventhSenseAI@users.noreply.github.com>
2024-06-12 16:38:18 +0800

    Add files via upload

---
+++ android/app/src/main/kotlin/ai/seventhsense/agent7/MainActivity.kt
@@ -0,0 +1,6 @@
+package ai.seventhsense.opencvfr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/fintech_seventhsense_facerecognition_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fintech_seventhsense_facerecognition_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
